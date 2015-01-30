ee32fd1ed85f517e5b823ed3d4d313c73f35d40c
immber & shambu1985 <pair+immber.shambu1985@devbootcamp.com>
2015-01-30 10:58:44 -0800

    added seeds

--- source/cards_against_sq/db/seeds.rb
+++ source/cards_against_sq/db/seeds.rb
@@ -3,6 +3,24 @@ require_relative 'config/application'
 def populate_white_cards
   white_cardsd= []
   file = File.open('wcards.txt', 'r') do |file|
-
+      while line = file.gets
+        white_cards << line
+      end
   end
-  File.readlines { |file| }
+  white_cards.each {|card| WhiteCard.create(text: card) }
+end
+
+
+def populate_black_cards
+  black_cards= []
+  file = File.open('bcards.txt', 'r') do |file|
+      while line = file.gets
+        black_cards << line
+      end
+  end
+  black_cards.each {|card| BlackCard.create(text: card) }
+end
+
+populate_white_cards
+populate_black_cards
+
