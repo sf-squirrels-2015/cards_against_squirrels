9f02fc0cd0deb07d48f4cf39958f95f2fc8064d7
ridou <richard.santin@gmail.com>
2015-01-30 16:29:54 -0800

    cards against squirrels is working!!

--- source/cards_against_sq/app/controllers/squirrel_cards.rb
+++ source/cards_against_sq/app/controllers/squirrel_cards.rb
@@ -1,5 +1,5 @@
-require_relative 'config/application'
-
+require_relative '../../config/application'
+require 'debugger'
 class Controller
   attr_accessor :game_over, :view, :players
 
@@ -10,22 +10,25 @@ class Controller
   end
 
   def load_game
+    @view.welcome
+
+    # debugger
     reset
     4.times do
       @players << Player.create(name: Faker::Name.name, points: 0)
     end
+    @players.each {|player| player.create_initial_hand }
   end
 
   def turn
+    # @view.clear_screen
     @view.show_hand(players[0].hand)
     read_black_card
-    @view.read_card(card)
     cards_played = played_cards
     winning_card = pick_winning_card(cards_played)
     show_winner(winning_card)
     check_points
     players_draw if !@game_over
-
   end
 
   def reset
@@ -41,8 +44,14 @@ class Controller
 
   def played_cards
     played_cards = []
-    played_cards << (WhiteCard.find(choose_card))
-    @players.each_with_index do |player, index|
+    choosen_card_id = @view.choose_card
+    if @players[0].hand.any? {|card| card.id == choosen_card_id }
+      played_cards << (WhiteCard.find(choosen_card_id))
+    else
+      @view.wrong_card
+      played_cards()
+    end
+     @players.each_with_index do |player, index|
       if index != 0
         played_cards << player.hand.sample
       end
@@ -57,14 +66,18 @@ class Controller
   end
 
   def show_winner(winning_card)
-    @view.read_card(@black_card)
-    @view.read_card(winning_card)
+    @view.read_winner(winning_card, @black_card)
+    @black_card.update_attributes(player_id: winning_card.player_id)
+    winning_player = @players.find{ |player| player.id == winning_card.player_id}
+    winning_player.update_attributes(points: (winning_player.points + 1))
   end
 
   def check_points
-    Player.all.each do |player|
+    @view.show_score(@players)
+    @players.each do |player|
       if player.winner?
         @game_over = true
+        @view.winning_player(player)
         break
       end
     end
@@ -82,12 +95,11 @@ end
 
 controller = Controller.new
 
+
 controller.load_game
+controller.turn
 
-until controller.game_over == false
-  turn
+until controller.game_over == true
+  controller.turn
+  sleep(5)
 end
-
-controller.reset
-
-
